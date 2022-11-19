e7e8b4eb5f923a015e86c6e28f6218620653440c
MariaArshakyan <117913837+MariaArshakyan@users.noreply.github.com>
2022-11-19 21:34:47 +0400

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,33 @@
+\\1
+let a
+let b
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+\\2
+let name='Maria'
+console.log('My name is '+name)
+let age=18;
+console.log('I am '+age)
+
+
+\\3
+let number=2
+if(number<0){console.log('yes')}
+else{console.log('no')}
+
+
+\\4
+let number0
+if(number0%2==0){console.log('even')}
+else{console.log('odd')}
+
+
+let number1
+let number2
+if(number1/number2===0||number2/number1===0){console,log(1)}
+else{console.log(0)}
